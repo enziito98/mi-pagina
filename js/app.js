2163279962d6329e7ee3ee324adff5e01d36db37
lenchi <enzoleonelsosa98@gmail.com>
2022-07-24 16:48:14 -0300

    nuevo_blog

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const enlaces = document.getElementsByClassName('nav--list')[0];
+const hamburguesa = document.getElementsByClassName('hamburguesa')[0];
+const menuHamburguesa = document.getElementById('hamburguesa');
+let abierto = false;
+
+const toggleMenu = () =>{
+    enlaces.classList.toggle('enlaces2');
+    enlaces.style.transition = 'transform 0.5s ease-in-out';
+}
+
+hamburguesa.addEventListener('click', function(){
+    toggleMenu();
+    if(document.querySelector('.nav--list.enlaces2')){
+        abierto=true;
+    }else{
+        abierto=false;
+    }
+})
+
+window.addEventListener('click', function(e){
+    this.console.log(e.target);
+    if(abierto){
+        if(e.target !== menuHamburguesa){
+            toggleMenu();
+            abierto=false;
+        }
+    }
+})
+
+window.addEventListener('resize', function(){
+    if(screen.width > 800){
+        if(abierto){
+            toggleMenu();
+            enlaces.style.transition = 'none';
+            abierto = false;
+        }
+    }
+})
